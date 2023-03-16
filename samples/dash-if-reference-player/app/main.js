5a26c0aa23a0673a1b95bddd14a6a68a693771cf
Hema Reddy <hemareddymuppidi@gmail.com>
2023-03-16 11:25:30 -0400

    modified

--- samples/dash-if-reference-player/app/main.js
+++ samples/dash-if-reference-player/app/main.js
@@ -35,7 +35,7 @@ angular.module('DashIFTestVectorsService', ['ngResource']).factory('dashifTestVe
 
 app.controller('DashController', ['$scope', '$window', 'sources', 'contributors', 'dashifTestVectors', function ($scope, $window, sources, contributors, dashifTestVectors) {
     $scope.selectedItem = {
-        url: 'https://nustreaming.github.io/streaming/bbb.mpd'
+        url:'https://nustreaming.github.io/streaming/bbb.mpd'
     };
 
     sources.query(function (data) {
